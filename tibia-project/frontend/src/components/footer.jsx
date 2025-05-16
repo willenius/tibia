a544cd9faadb41c416800d7a50f9075f2fb3b462
willenius <wilhelm.bjor@gmail.com>
2025-05-16 12:26:50 +0200

    sista commit för idag. fortsätta sätta strukturen för header och footer framöver. sedan göra en register &inloggningspage

---
+++ tibia-project/frontend/src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer className="footerBtm">
+        <div>
+          <p>About tibia</p>
+          <p>Terms & Conditions</p>
+          <p>©2025, tibia4u. All rights reserved 4u.</p>
+          <p> Social links</p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ tibia-project/frontend/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+    return(
+        <>
+        <header className="headerTop">
+            <div>
+                <p>tibia</p>
+                <p>Shows & Tickets</p>
+                <p>Videos</p>
+            </div>
+        </header>
+        </>
+    );
+}
+
+export default Header;
