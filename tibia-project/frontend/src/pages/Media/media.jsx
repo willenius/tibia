97154dca4dd9486339008777a794ac1d5dd5647c
willenius <wilhelm.bjor@gmail.com>
2025-05-27 11:24:09 +0200

    pushar och byter branch

--- tibia-project/frontend/src/pages/Media/media.jsx
+++ tibia-project/frontend/src/pages/Media/media.jsx
@@ -1,26 +1,23 @@
 import React from "react";
-import ReactPlayer from "react-player/youtube";
-import './/media.css';
-
+import ReactPlayer from "react-player/youtube"; //kommer ta bort sen, ej använda mao
+import { Player } from "video-react";
+import ".//media.css";
+import "video-react/dist/video-react.css";
 
 function Media() {
   return (
     <div className="flexContainerMedia">
       <div className="mediaGrid">
-        <video width="640" height="360" controls> 
-        <source src="/marys.mp4" type="video/mp4" /> 
-        </video>
-        <ReactPlayer
-          url="https://youtu.be/9ez0NA3GFxY?si=ATHRdsx65_Gs2eIs-U"
+        <Player
+          playsInline
+          fluid={false} //tar bort vide-react standard css för videon. aint pretty men något
+          width={640}
+          height={360}
+          src="/marys.mp4"
         />
-        <ReactPlayer
-          url="https://youtu.be/fDwZG0_aK-0?si=EZqg3P1Eq6PS-Vgf-U"
-          
-        />
-        <ReactPlayer
-          url="https://youtu.be/OlnZ1JOZM9s?si=A0cHA5DML-LjPtKw-U"
-         
-        />
+         {/* <ReactPlayer url="https://youtu.be/9ez0NA3GFxY?si=ATHRdsx65_Gs2eIs-U" /> */}
+        {/* <ReactPlayer url="https://youtu.be/fDwZG0_aK-0?si=EZqg3P1Eq6PS-Vgf-U" /> */}
+        <ReactPlayer url="https://youtu.be/OlnZ1JOZM9s?si=A0cHA5DML-LjPtKw-U" /> 
         {/*  /* göra en egen sida som är som en video-player, nu react-player så länge...   */}
       </div>
     </div>
