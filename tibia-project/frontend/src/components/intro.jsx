bc03610f537519ea096f80b2d2a5a872ed50df77
willenius <wilhelm.bjor@gmail.com>
2025-06-11 11:52:35 +0200

    la till components.json + tailwind config vilket jag tror är lite i onödan. nåväl, har kvar det - pushar och byter branch.

--- tibia-project/frontend/src/components/intro.jsx
+++ tibia-project/frontend/src/components/intro.jsx
@@ -3,36 +3,23 @@ import { IoPlay } from "react-icons/io5";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import tibiaLogo from "../assets/tibia.svg";
-import { useEffect, useState } from "react";
-import * as React from "react";
-// import { Progress } from "@/components/ui/progress"; ((lista ut detta))
 
 function Intro() {
-  const [progress, setProgress] = React.useState(13);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <>
-      <div className="introWrapper">
-        <Link to="/">
-          <motion.img
-            src={tibiaLogo}
-            alt="logo of tibia"
-            className="tibiaLogo"
-            animate={{
-              scale: 1,
-              rotate: 360,
-              transition: { duration: 1 },
-            }}
-          />
-          {/* <Progress value={progress} className="w-[60%]" /> lista ut*/}
-        </Link>
-      </div>
-    </>
+    <div className="introWrapper">
+      <Link to="/">
+        <motion.img
+          src={tibiaLogo}
+          alt="logo of tibia"
+          className="tibiaLogo"
+          animate={{
+            scale: 1,
+            rotate: 360,
+            transition: { duration: 1 },
+          }}
+        />
+      </Link>
+    </div>
   );
 }
 
